1b10001e25142b4f04d90d6f6955df83ad17f4cc
SultanKodz <sultankodzoev06@mail.ru>
2023-12-18 22:51:41 +0300

    Добавлен звук при нажатии на кнопку PLAY

--- main_window.py
+++ main_window.py
@@ -1,16 +1,33 @@
 # СОЗДАНИЕ ГЛАВНОГО МЕНЮ
 from PIL import Image
 import pygame
+import map
+
+pygame.mixer.pre_init(44100, -16, 1, 512)
+import os
+import sys
 
 # КОНСТАНТЫ
 SIZE = WIDTH, HEIGHT = 1400, 788
 BACKROUND_IMAGE = "Images/backround.gif"
 BACKROUND_MUSIC = "Music/backround_music.mp3"
+PLAY_BUTTON = "play_button.png"
+CLICK_SOUND = "Music/click.mp3"
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('Images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
 # Класс главного меню
 class Menu:
-    def __init__(self, size, bg_image, sound):  # объект принимает размеры окна, фоновое изображение и фоновую музыку
+    def __init__(self, size, bg_image):  # объект принимает размеры окна, фоновое изображение и фоновую музыку
         # Параметры окна
         self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Santa Race")
@@ -18,9 +35,9 @@ class Menu:
         # Всякие нужные значения
         self.x, self.y = self.size = size
         self.bg_image = bg_image
-        self.sound = sound
         self.im = Image.open(self.bg_image)
         self.bg = pygame.image.load(self.bg_image)
+        self.sound = pygame.mixer.Sound(BACKROUND_MUSIC)
 
     def load_bg(self):
         # Загрузка заднего фона
@@ -28,18 +45,44 @@ class Menu:
 
     def play_music(self):
         # Проигрыватель музыки главного меню
-        pygame.mixer.music.load(self.sound)
-        pygame.mixer.music.play(-1, 0.5)
+        self.sound.play(-1)
+        self.sound.set_volume(0.4)
+
+
+all_sprites = pygame.sprite.Group()
+
+
+class Play(pygame.sprite.Sprite):
+    button_play = load_image(PLAY_BUTTON)
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = Play.button_play
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDTH // 2 - 200
+        self.rect.y = HEIGHT // 2 - 150
+        self.sound = pygame.mixer.Sound(CLICK_SOUND)
+
+    def update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
+                self.rect.collidepoint(args[0].pos):
+            self.sound.play(0)
 
 
 if __name__ == '__main__':
     pygame.init()
-    menu = Menu(SIZE, BACKROUND_IMAGE, BACKROUND_MUSIC)
+    menu = Menu(SIZE, BACKROUND_IMAGE)
     menu.play_music()
     menu.load_bg()
+    button_play = Play(all_sprites)
+    all_sprites.draw(menu.screen)
     running = True
     while running:
+        all_sprites.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                all_sprites.update(event)
+
         pygame.display.flip()
